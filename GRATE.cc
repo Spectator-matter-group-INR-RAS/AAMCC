093e8035011e77ead6c128a12cac6e11301e028e
nepeivodaRS <nepeyvoda.rs@phystech.edu>
2021-11-25 14:51:06 +0100

    Revert "Update GRATE.cc"
    This reverts commit 396d63e3a86d62287f9eaf63527d1372fcdd97af.

--- GRATE.cc
+++ GRATE.cc
@@ -260,7 +260,7 @@ int main()
 
     NucleonVector nV;
     GlauberCollisionReader reader;
-    FermiMomentum FermiMom(&nV, "G");
+    FermiMomentum FermiMom(&nV, "M");
     FermiMom.SetPzPerNucleon(histoManager.GetInitialContidions().GetPzA()/ sourceA, histoManager.GetInitialContidions().GetPzB() / sourceAb);
 
     for(G4int count=0;count<histoManager.GetIterations() ;count++){ 
