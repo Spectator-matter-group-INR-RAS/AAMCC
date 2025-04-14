a9eaf49a4aaffb78c4d0bc782b441b4412abc0e4
alexsvetlichnyy <aleksandr.svetlichnyy@phystech.edu>
2025-04-15 00:50:52 +0300

    add Ne-22 nuclei

--- src/InitialConditions.cc
+++ src/InitialConditions.cc
@@ -26,6 +26,7 @@ G4bool InitialConditions::SetSysA(G4String SysA_in) {
     else if(SysA_in == "Opgcm"){sourceA = 16; sourceZ = 8; SysA = SysA_in;}
     else if(SysA_in == "Oho"){sourceA = 16; sourceZ = 8; SysA = SysA_in;}
     else if(SysA_in == "Ne"){sourceA = 20; sourceZ = 10; SysA = SysA_in;}
+    else if(SysA_in == "Ne22"){sourceA = 22; sourceZ = 10; SysA = SysA_in;}
     else if(SysA_in == "Necl"){sourceA = 20; sourceZ = 10; SysA = SysA_in;}
     else if(SysA_in == "Au"){sourceA = 197; sourceZ = 79; SysA = SysA_in;}
     else if(SysA_in == "Aurw"){sourceA = 197; sourceZ = 79; SysA = SysA_in;}
@@ -73,6 +74,7 @@ G4bool InitialConditions::SetSysB(G4String SysB_in) {
     else if(SysB_in == "Opgcm") {sourceAb = 16; sourceZb = 8; SysB = SysB_in;}
     else if(SysB_in == "Oho") {sourceAb = 16; sourceZb = 8; SysB = SysB_in;}
     else if(SysB_in == "Ne") {sourceAb = 20; sourceZb = 10; SysB = SysB_in;}
+    else if(SysB_in == "Ne22") {sourceAb = 22; sourceZb = 10; SysB = SysB_in;}
     else if(SysB_in == "Necl") {sourceAb = 20; sourceZb = 10; SysB = SysB_in;}
     else if(SysB_in == "Au"){sourceAb = 197; sourceZb = 79; SysB = SysB_in;}
     else if(SysB_in == "Aurw"){sourceAb = 197; sourceZb = 79; SysB = SysB_in;}
